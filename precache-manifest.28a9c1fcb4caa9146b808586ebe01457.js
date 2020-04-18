0bc641d82c038c007c745d7600c3bb9b701be35f
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-04-18 18:47:39 +0530

    Updates

---
+++ precache-manifest.28a9c1fcb4caa9146b808586ebe01457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181a092a7aec6bca75433f8810281522",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/index.html"
+  },
+  {
+    "revision": "ee5e8880f29b72bed45e",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ed8069e28d823ce642e4",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/css/main.68a2d3fb.chunk.css"
+  },
+  {
+    "revision": "ee5e8880f29b72bed45e",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/js/2.63d7c07f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/js/2.63d7c07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8069e28d823ce642e4",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/js/main.8164e649.chunk.js"
+  },
+  {
+    "revision": "0d78256d1575e34f2293",
+    "url": "/1ayushgoyal007.github.io/get-recipe-app/static/js/runtime-main.ea702f02.js"
+  }
+]);
